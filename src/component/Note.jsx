3c0cb4ddaf91ed5daf705e225801f9ab5b489d14
Fiona Leung <oleung8@myseneca.ca>
2023-11-21 18:02:17 -0500

    Init commit for the basic interface

---
+++ src/component/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Note() {
+    return (
+        <div className="note">
+            <h1>title</h1>
+            <p>content</p>
+        </div>
+    );
+}
